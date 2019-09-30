4ca8f2ded2cb5675dcc527fc8eb6222aee53e670
herbertcordeiro <herbert.cordeiro@dce.ufpb.br>
2019-09-30 14:30:04 -0300

    add app recursos

--- core/models.py
+++ core/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from recursos.models import Recursos
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+    recursos = models.ManyToManyField(Recursos)
 
     def __str__(self):
         return self.nome
